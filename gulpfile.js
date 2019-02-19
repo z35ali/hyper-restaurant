c984f93f8328755c4c03bc261e365b281fe9f8f5
Zafar <zafar.a.ali2@gmail.com>
2019-02-18 23:26:17 -0500

    Github Deploy

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,16 @@ const browserSync = require('browser-sync')
 const reload = browserSync.reload
 var exec = require('child_process').exec;
 
+var deploy = require('gulp-gh-pages');
+
+  gulp.task('deploy', function () {
+    return gulp.src("./prod/**/*")
+      .pipe(deploy({ 
+        remoteUrl: "https://github.com/z35ali/hyper-restaurant.github.io.git",
+        branch: "master"
+      }))
+  });
+
 gulp.task('default', ['styles', 'webpack', 'browser-sync'], () => {
   gulp.watch('./assets/sass/**/*', ['styles'])
   gulp.watch('./assets/js/**/*', ['webpack'])
